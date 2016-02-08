e0f3448e5704eef1fc74ecfe0be1801166d05301
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-02-08 16:59:17 +0300

    One more Python code, drawing flowers eh

---
+++ t_flower_round.py
@@ -0,0 +1,20 @@
+import turtle
+from math import sin, pi
+
+turtle.shape('turtle')
+
+n = 50
+r = 20
+double_leaf = 3 #количество лепестков по два (180/delta)
+delta = 0
+phi = 180*(1-2/n)
+a = 2*r*sin(pi/n)
+
+for angle in range(double_leaf):
+	turtle.seth(delta)
+	for leaf in range (2):
+		for i in range(n):
+			turtle.forward(a)
+			turtle.left(180-phi)
+		phi = 360-phi #смена угла
+	delta += 60 #угол поворота
