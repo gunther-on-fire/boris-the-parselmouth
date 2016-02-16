ca3fd66361c78c586babb2383a05650101e40e29
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-02-16 09:37:28 +0300

    Added the correct position of the center

--- star.py
+++ star.py
@@ -1,12 +1,25 @@
 import turtle
+from math import pi, cos
 
 turtle.shape("turtle")
 
+turtle.speed(1)
+
 def star(r,n):
+	angle = 180/n
+	l = 2*r*cos(pi/(2*n))
+
+	turtle.penup()
+	turtle.seth(90)
+	turtle.forward(r)
+	turtle.right(180-angle/2)
+	turtle.pendown()
+
 	for i in range(n):
-		angle = 180/n
-		turtle.forward(r)
+		turtle.forward(2*r)
 		turtle.right(180-angle)
 
-r = 100
+r = 150
+
 star(r,11)
+turtle.goto(0,0)
