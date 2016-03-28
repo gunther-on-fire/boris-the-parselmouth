df5bdbb7dd507423ab802a242419fab9dc05e406
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-03-28 12:14:15 +0300

    The first and the last element conditions were taken out from for cycle

--- lab5-Lists/4-3_one_element_ver2.py
+++ lab5-Lists/4-3_one_element_ver2.py
@@ -1,6 +1,5 @@
 A = [1,2,2,3,3,4,0]
 A.sort()
-prev_el = None
 
 if A[1] != A[0]:
     print(A[0])
@@ -8,7 +7,7 @@ if A[-1] != A[-2]:
     print(A[-1])  
 
 for i in range(1, len(A)-1):		
-	if (A[i] != prev_el and A[i] != A[i+1]):
+	if (A[i] != A[i-1] and A[i] != A[i+1]):
 		print(A[i])
 	prev_el = A[i]
 
